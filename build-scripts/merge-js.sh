121b9a4c0627e06aaed6a53fdfe53c86a4a518da
Dan Smith <dev@dotherightthing.co.nz>
2020-12-28 13:35:34 +1300

    Use file lists to populate frontend.js and backend.js

--- build-scripts/merge-js.sh
+++ build-scripts/merge-js.sh
@@ -18,4 +18,69 @@
 # v: print shell input lines as they are read (including all comments!)
 set -e
 
-cat ./js/classes/_config.js ./js/classes/*.js ./js/init.js > ./js/frontend.js
+# https://www.cyberciti.biz/faq/bash-read-file-names-from-a-text-file-and-take-action/
+# IFS = Internal Field Separator
+# -r = newline
+# file = variable name
+
+# ===== BACKEND =====
+
+backendJs="${1:-./js/backend.js}"
+backendSrc="${1:-./js/backend.txt}"
+backendTimestamp=$(date +"/* backend.js - generated %d/%m/%Y at %T from: */")
+index=0
+
+# if backendSrc doesn't exist
+if [ ! -f "$backendSrc" ]; then
+    echo "$0 - File $backendSrc not found.";
+    exit 1;
+fi
+
+# if backendJs exists empty it, otherwise create it
+echo "${backendTimestamp}" > "${backendJs}"
+
+# read file list in backendSrc, copy filename to backendJs
+while IFS= read -r file
+do
+    index=$(($index+1))
+    echo "/* $index $file */" >> "${backendJs}"
+done < "${backendSrc}"
+
+echo "\r" >> "${backendJs}"
+
+# read file list in backendSrc, copy contents to backendJs
+while IFS= read -r file
+do
+    cat $file >> "${backendJs}"
+done < "${backendSrc}"
+
+# ===== FRONTEND =====
+
+frontendJs="${1:-./js/frontend.js}"
+frontendSrc="${1:-./js/frontend.txt}"
+frontendTimestamp=$(date +"/* frontend.js - generated %d/%m/%Y at %T from: */")
+index=0
+
+# if frontendSrc doesn't exist
+if [ ! -f "$frontendSrc" ]; then
+    echo "$0 - File $frontendSrc not found.";
+    exit 1;
+fi
+
+# if frontendJs exists empty it, otherwise create it
+echo "${frontendTimestamp}" > "${frontendJs}"
+
+# read file list in frontendSrc, copy filename to frontendJs
+while IFS= read -r file
+do
+    index=$(($index+1))
+    echo "/* $index $file */" >> "${frontendJs}"
+done < "${frontendSrc}"
+
+echo "\r" >> "${frontendJs}"
+
+# read file list in frontendSrc, copy contents to frontendJs
+while IFS= read -r file
+do
+    cat $file >> "${frontendJs}"
+done < "${frontendSrc}"
